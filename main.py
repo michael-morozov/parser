1e254e1ab13bd73a2e9f016670b47c43972eb3b4
MichaelM <2018michael.morozov@gmail.com>
2023-04-24 16:34:56 +0700

    Update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import chardet
+
 
 # url = 'http://nxcoreapi.com/symbols.php?search=alti&m_exchange=AMEX&m_type=e&s_type=contains&m_symbol=on&m_name=on'
 # url = 'http://nxcoreapi.com/symbols.php?search=&m_exchange=&m_type=e&s_type=contains&m_symbol=on&m_name=on'
@@ -8,9 +10,14 @@ import csv
 url = input("Please enter URL: ")
 filename=input("Please type filename(WO /'.csv/' extension): ")
 
+
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 table = soup.find('table')
+
+headers = []
+for th in table.find_all('th'):
+    headers.append(th.text)
 
 data = []
 for tr in table.find_all('tr'):
@@ -21,13 +28,19 @@ for tr in table.find_all('tr'):
 
 filename += ".csv"
 
-
-with open(filename, 'w', newline='') as csvfile:
+with open(filename,'w', encoding = '1251', newline='') as csvfile:
     writer = csv.writer(csvfile)
+    if headers != []:
+        writer.writerow(headers)
     for row in data:
-        writer.writerow(row)
+        if row != []:
+            writer.writerow(row)
+            # print("row - ", row)
 
 len = len(data)-1
-
 print("done, {d} rows saved (WO Header row)".format(d=len))
 input("Press any key to exit\n")
+
+
+
+
