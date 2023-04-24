26cac11598ec8da33dd12c66a2f6028bdda77c4e
MichaelM <2018michael.morozov@gmail.com>
2023-04-24 13:04:22 +0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+# url = 'http://nxcoreapi.com/symbols.php?search=alti&m_exchange=AMEX&m_type=e&s_type=contains&m_symbol=on&m_name=on'
+url = 'http://nxcoreapi.com/symbols.php?search=&m_exchange=&m_type=e&s_type=contains&m_symbol=on&m_name=on'
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+table = soup.find('table')
+
+data = []
+for tr in table.find_all('tr'):
+    row = []
+    for td in tr.find_all('td'):
+        row.append(td.text)
+    data.append(row)
+
+with open('table.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    for row in data:
+        writer.writerow(row)
+
+len = len(data)-1
+
+print("done, {d} rows saved (WO Header row)".format(d=len))
